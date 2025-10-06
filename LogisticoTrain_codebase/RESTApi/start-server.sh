1547161bec2a76b233ab8056494e0369f79b9688
Thomas Friquet <i2302083@cri.univ-lemans.fr>
2025-10-06 17:16:28 +0200

    rr

--- LogisticoTrain_codebase/RESTApi/start-server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "START SERVER..."
-# -u is important to not buffer console I/O that could cause no showing output to console in some docker platform (macos especially)
-python -m MyRamesServer $@
